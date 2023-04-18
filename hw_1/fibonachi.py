de8c736937691979d7d23404d76d06f60dba19af
demin <demin132132@mail.ru>
2023-04-19 02:10:48 +0300

    medium

--- hw_1/fibonachi.py
+++ hw_1/fibonachi.py
@@ -4,16 +4,3 @@ def fibonachi(x: int):
     elif x == 1:
         return 1
     return fibonachi(x-1) + fibonachi(x-2)
-
-
-def main():
-    res_1 = fibonachi(20)
-    res_2 = fibonachi(22)
-    assert res_1 == 6765 and res_2 == 17711
-
-    with open('./artifacts/res.txt', 'w') as f:
-        f.write(f'{res_1}, {res_2}')
-
-
-if __name__ == '__main__':
-    main()
